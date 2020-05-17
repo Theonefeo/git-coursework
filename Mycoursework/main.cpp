a5dc26e8ac52357a200ffbf955944d082cac9330
Феодор Карпов <fedorkarpov1312@gmail.com>
2020-05-17 22:52:53 +0300

    Работа над курсовой.Продолжение №1. Реализовал деление файла(f.txt) на подфайлы(f1.txt и f2.txt) с помощью массивов и вектора. В дальнейшем реализую сортировку подфайлов.

--- Mycoursework/main.cpp
+++ Mycoursework/main.cpp
@@ -3,26 +3,96 @@
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <vector>
+#include <cstring>
+#include <string>
 using namespace std;
 //Запись в файл рандомных чисел
 void file()
 {
-   int bb;
-   cin>>bb;
-  int b[bb];
-  ofstream fout("f.txt");
-  for(int i=0;i<bb;i++)
-  {
-    b[i]=rand()%10;
-    fout<<b[i];
-  }
-  fout.close();
+    int bb;
+    cin>>bb;
+    int b[bb];
+    ofstream fout("f.txt");
+    for(int i=0; i<bb; i++)
+    {
+        b[i]=rand()%10;
+        fout<<b[i]<<endl;
+    }
+    fout.close();
+}
+//Функция разделения "большого" файла на массивы
+void Splitting_a_File()
+{
+    int s[50];
+    int ss[100];
+    //Запись содержимого файла в массив для того,чтобы потом из массивa распределить элементы по файлам
+    ifstream fin("f.txt");
+    //vector<int>v;
+    //пока файл не пуст проходим по нему
+    while(!fin.eof())
+    {
+
+        for(int i=0; i<50; i++)
+        {
+            //string s;
+            //getline(ifs, s);
+            fin>>s[i];
+        }
+        for(int i=50; i<100; i++)
+        {
+            //string s;
+            //getline(ifs, s);
+            fin>>ss[i];
+        }
+    }
+    fin.close();
+    cout<<endl;
+    //cout<<"f2"<<endl;
+    //int sf2[50];
+    vector<int>v;
+    int as;
+    //Копирование полунормального массива в нормальный
+    for (int i = 50; i <100; i++)
+    {
+
+        as = ss[i];
+       v.push_back(as);
+    }
+    int sf2[50];
+    for (int i = 0; i <50; i++)
+    {
+
+
+       sf2[i] = v.at(i);
+    }
+
+    //перенос элементов из массива в доп файл f1
+    ofstream fout("f1.txt");
+    for(int i = 0; i<50; i++)
+    {
+
+        fout<<s[i]<<" ";
+
+
+    }
+    fout.close();
+    //перенос элементов из массива в доп файл f2
+    ofstream fout1("f2.txt");
+    for(int i = 0; i<50; i++)
+    {
+
+        //fout1<<ss[i]<<" ";
+        fout1<<sf2[i]<<" ";
+        //v.pop_back();
+    }
+
+    fout1.close();
+
 }
 void Vvod(int n, float* a)//создание массива
 {
-    int i;
-
-    for(i=0; i<n; i++)//рандомное заполнение массива
+    for(int i=0; i<n; i++)//рандомное заполнение массива
     {
         a[i]=rand()%10;
     }
@@ -50,10 +120,10 @@ void Sortirovka(int n, float* a)///сортировка выборочная
         imin = i;
         for(int j=i+1; j<n; j++)
         {
-             if(a[j]<a[imin])///если текущий элемент меньше минимума
-        {
-            imin = j;
-        }
+            if(a[j]<a[imin])///если текущий элемент меньше минимума
+            {
+                imin = j;
+            }
         }
 
         swap(a[i], a[imin]);///меняем местами текущий элемент с минимальным
@@ -63,141 +133,41 @@ void Sortirovka(int n, float* a)///сортировка выборочная
 }
 int main()
 {
- //fstream file("f.txt");
-    //if (!file.is_open()) cout << "Not open\n"; // если не открылся
-    //else if (file.peek() == EOF) cout << "Fail empty\n"; // если первый символ конец файла
+    //cout<<"Please inter the element in the file ";
+    //file();
+    //проверка на то, пустой ли файл или нет
     const char filename[] = "f.txt";
 
     FILE* f = fopen(filename, "rb");
     if(!f)
     {
-        cout << "File is not exists" << endl;
-        return 0;
+        cout << "File is not exists" << endl;// файл не найден
+
     }
 
     char h;
     size_t readed = fread(&h, 1, 1, f);
 
     if(readed)
-        cout << "File contains data" << endl;
+        cout << "File contains data" << endl; // в файле находятся данные
     else
-        cout << "File NOT contains data" << endl;
+        cout << "File NOT contains data" << endl; //в файле не находятся данные - файл пустой
 
 
 
-   //int bb;
-   //cout<<"Please inter the element in the file ";
-   //cin>>bb;
-   //file();
-
-
-    int n;
-    float a[100];
-    cout<<"Inter n ";
-    cin>>n;
-    cout<<endl;
-    Vvod(n, a);
-    Vyvod(n, a);
-    cout<<"Sorted array "<<endl;
-    Sortirovka(n, a);
-    Vyvod(n, a);
-    //double q;
-    //cout<<"\n"<<"Inter q "<<"\n";
-    //cin>>q;
+    //int n;
+    //float a[100];
+    //cout<<"Inter n ";
+    //cin>>n;
     //cout<<endl;
+    //Vvod(n, a);
+    //Vyvod(n, a);
+    //Разбивка файла на части(на другие файлы меньшего размера)
+    Splitting_a_File();
+    /*int d[10];
 
-
-  return 0;
+    //cout<<"Sorted array "<<endl;
+    //Sortirovka(n, a);
+    //Vyvod(n, a);*/
+    return 0;
 }
-
-
-
-//    srand(time(0));
-    /*void Simple_Merging_Sort (
-
-{
-    char *name;
-  int a1, a2, k, i, j, kol, tmp;
-  ifstream fin("f.txt");
-
-  FILE *f, *f1, *f2;
-
-  kol = 0;
-  if ( (f = fopen(name,"r")) == NULL )
-    printf("\nИсходный файл не может быть прочитан...");
-  else {
-    while ( !feof(f) ) {
-      fscanf(f,"%d",&a1);
-      kol++;
-    }
-    fclose(f);
-  }
-  k = 1;
-  while ( k < kol ){
-    f = fopen(name,"r");
-    f1 = fopen("smsort_1","w");
-    f2 = fopen("smsort_2","w");
-    if ( !feof(f) ) fscanf(f,"%d",&a1);
-    while ( !feof(f) ){
-      for ( i = 0; i < k && !feof(f) ; i++ ){
-        fprintf(f1,"%d ",a1);
-        fscanf(f,"%d",&a1);
-      }
-      for ( j = 0; j < k && !feof(f) ; j++ ){
-        fprintf(f2,"%d ",a1);
-        fscanf(f,"%d",&a1);
-      }
-    }
-    fclose(f2);
-    fclose(f1);
-    fclose(f);
-
-    f = fopen(name,"w");
-    f1 = fopen("smsort_1","r");
-    f2 = fopen("smsort_2","r");
-    if ( !feof(f1) ) fscanf(f1,"%d",&a1);
-    if ( !feof(f2) ) fscanf(f2,"%d",&a2);
-    while ( !feof(f1) && !feof(f2) ){
-      i = 0;
-      j = 0;
-      while ( i < k && j < k && !feof(f1) && !feof(f2) ) {
-        if ( a1 < a2 ) {
-          fprintf(f,"%d ",a1);
-          fscanf(f1,"%d",&a1);
-          i++;
-        }
-        else {
-          fprintf(f,"%d ",a2);
-          fscanf(f2,"%d",&a2);
-          j++;
-        }
-      }
-      while ( i < k && !feof(f1) ) {
-        fprintf(f,"%d ",a1);
-        fscanf(f1,"%d",&a1);
-        i++;
-      }
-      while ( j < k && !feof(f2) ) {
-        fprintf(f,"%d ",a2);
-        fscanf(f2,"%d",&a2);
-        j++;
-      }
-    }
-    while ( !feof(f1) ) {
-      fprintf(f,"%d ",a1);
-      fscanf(f1,"%d",&a1);
-    }
-    while ( !feof(f2) ) {
-      fprintf(f,"%d ",a2);
-      fscanf(f2,"%d",&a2);
-    }
-    fclose(f2);
-    fclose(f1);
-    fclose(f);
-    k *= 2;
-  }
-  remove("smsort_1");
-  remove("smsort_2");
-  fin.close();
-
-return 0; */
