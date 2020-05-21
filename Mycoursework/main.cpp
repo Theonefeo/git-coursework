c724c11604e99e4f15fb6c418e12fb2841414cba
Феодор Карпов <fedorkarpov1312@gmail.com>
2020-05-22 01:51:48 +0300

    Работа над алгоритмом. Ошибка связанная  с обращением за пределы массивы исправлены, но тем не менее возникает ошибка в 158, 170, 174 : "merge was not declared in this scope ". Я понимаю что функция записи массива С(writeFromFrrayC) просто не видит функцию слияния merge но как исправить данное недоразумение, пока не знаю. Так же учел случай при помещение элементов из массивов а и б такие, когда элементы этих массивов равны - собственно  из-за этого добавлена новая функция(writeFromFrrayC) для удобства(и из-за этого ошибка с прочтением поэтапным функций).

--- Mycoursework/main.cpp
+++ Mycoursework/main.cpp
@@ -75,9 +75,10 @@ void writeToFile(string *& arr, int n, ofstream &fout)
 }
 /////////////////2 этап алгоритма
 //Чтение по 5 элементов из файла А в массив а
-string* readFromFileA(ifstream &finA, int n)
+void readFromFileA(ifstream &finA, int n, string *a) //OBYAVLENIE
 {
-    string *a = new string[n];
+    //string *a = new string[n];
+
     string buffA;
 
     for (int i = 0; !finA.eof() && i < n; i++)
@@ -87,12 +88,12 @@ string* readFromFileA(ifstream &finA, int n)
         a[i] = buffA;
 
     }
-    return a;
+
 }
 //Чтение по 5 элементов из файла Б в массив б
-string* readFromFileB(ifstream &finB, int n)
+void readFromFileB(ifstream &finB, int n, string *b)
 {
-    string *b = new string[n];
+    //string *b = new string[n];
     string buffB;
 
     for (int i = 0; !finB.eof() && i < n; i++)
@@ -102,7 +103,7 @@ string* readFromFileB(ifstream &finB, int n)
         b[i] = buffB;
 
     }
-    return b;
+
 }
 //Для тестирования
 void printA(string a[], int nn)
@@ -126,28 +127,9 @@ void printB(string b[], int nm)
     }
     cout << "]" << endl;
 }
-//Функция слияния элементов массива а и б в результирующий с(д)
-void merge(string *&a, int &i, string *&b, int &j, string *&c, int &p, int n) //, bool flag)
-{
-    //Слияние: идем по массивам а и b сравнивая элемемнты и заполняя массив с(д) который потом запишим в файл С(Д)
-    while (!(i == n || j == n || p == n*2))
-    {
-        if (a[i] < b[j])
-        {
-            c[p] = a[i]; //Место возникновения ошибки - после этой строчки программа вылетает - проверено Дебагерром(Segmentation fault (core dumped) - process returned 139(0x8B))
-            i++;
-            p++;
-        }
-        else
-        {
-            c[p] = b[j];
-            j++;
-            p++;
-        }
-    }
-}
+
 //Запись массива с в файл С
-void writeToFileC(string *& c, int n, ofstream &foutC)
+void writeToFileC(string * c, int n, ofstream &foutC)
 {
     for(int i=0; i<n; ++i)
     {
@@ -155,13 +137,87 @@ void writeToFileC(string *& c, int n, ofstream &foutC)
     }
 }
 //Запись массива д в файл Д
-void writeToFileD(string *& c, int n, ofstream &foutD)
+void writeToFileD(string * c, int n, ofstream &foutD)
 {
     for(int i=0; i<n; ++i)
     {
         foutD<<c[i]<<endl;
     }
 }
+//Функция выгрузки массива с в файлы С и Д
+void writeFromFrrayC(ofstream &foutC, ofstream &foutD, string *c,int N, string *a, int &i, string *b, int &j,int &p )
+{
+    int Np=0;
+    if(Np<2)
+    {
+        //записать массив c в файл С
+        writeToFileC(c, N, foutC);
+        p =0;
+        Np++;
+        //Заходим в функцию чтобы продолжить слияние при опустошенном массиве С
+        merge(a, i, b,j, c, p, N/2);
+    }
+    else if(Np>=2 && Np<4)
+    {
+        //записать массив d в файл D
+        writeToFileD(c, N, foutD);
+        p =0;
+        Np++;
+        if(Np>=4)
+        {
+            Np=0;
+            //Переходим к слиянию
+            merge(a, i, b,j, c, p, N/2);
+        }
+        else
+        //Переходим к слиянию
+            merge(a, i, b,j, c, p, N/2);
+    }
+}
+//Функция слияния элементов массива а и б в результирующий с(д)
+void merge(string *a, int &i, string *b, int &j, string *c, int &p, int n, ofstream &foutC, ofstream &foutD, int N) //, bool flag)
+{
+    //Слияние: идем по массивам а и b сравнивая элемемнты и заполняя массив с(д) который потом запишим в файл С(Д)
+    while (!(i == n || j == n || p == n*2))
+    {
+        if (a[i] < b[j])
+        {
+            if(p<n*2)
+            {
+                c[p] = a[i]; //Место возникновения ошибки - после этой строчки программа вылетает - проверено Дебагерром(Segmentation fault (core dumped) - process returned 139(0x8B))
+                i++;
+                p++;
+            }
+        }
+        else if(a[i] > b[j])
+        {
+            c[p] = b[j];
+            j++;
+            p++;
+        }
+        else if(a[i] == b[j])
+        {
+            c[p] = a[i];
+            i++;
+            p++;
+            if(p == n*2)
+            {
+                writeFromFrrayC(foutC, foutD, c, N, a, i, b, j,p );
+            }
+            else
+            {
+            c[p] = b[j];
+            j++;
+            p++;
+            if(p == n*2)
+            {
+                writeFromFrrayC(foutC, foutD, c, N, a, i, b, j,p );
+            }
+            }
+        }
+    }
+}
+
 int main()
 {
 
@@ -221,85 +277,85 @@ int main()
     fout1.close();
     fout2.close();*/
 //////////////////////////////// 2 этап реализации алгоритма
-    ifstream finA("A.txt"); //Открытие файла А для чтения
-    ifstream finB("B.txt"); //Открытие файла Б для чтения
+    //ifstream finA("A.txt"); //Открытие файла А для чтения
+    //ifstream finB("B.txt"); //Открытие файла Б для чтения
 
+    ifstream finA("A.txt");
+    if (!finA)
+    {
+        cerr << "Uh oh, file could not be opened for reading!" << endl;
+        return -1;
+    }
+
+    ifstream finB("B.txt");
+    if (!finB)
+    {
+        cerr << "Uh oh, file could not be opened for reading!" << endl;
+        return -1;
+    }
     ofstream foutC("C.txt"); //Открытие файла С для записи
     ofstream foutD("D.txt"); //Открытие файла Д для записи
 
-    string *a;
-    string *b;
-    string  *c;
-    //string  *d;
+    //string *a;
+    //string *b;
+    //string *c;
+
     //
     cout<<"enter the limit  ";
     int N;
     cin>>N;
 
-    a = readFromFileA(finA, N/2);
-    b = readFromFileB(finB, N/2);
-    int n;
-    int i = 0, j= 0, p=0, Np=0;
+    string a[N/2];
+    string b[N/2];
+    string c[N];
+
+    readFromFileA(finA, N/2, a);
+    readFromFileB(finB, N/2, b);
+    int n = N/2;
+    int i = 0, j= 0, p=0;
     //в цикле
 //Попеременное чтение из файлов А и В отрезков длиною 5(в массив а-5эл и в-5эл) и дальнейшее копирование слиянием в результирующие массивы с и d с записью их в соответсвующие файлы
     for(int pi=0; !finA.eof() && !finB.eof(); ++pi)
     {
-        merge(a, i, b,j, c, p, N/2);
+
+        merge(a, i, b,j, c, p, N/2, foutC, foutD, N);
 
         //Если количество элементов массива с равно 10 (ограничение на оп.память) то записываем  его в файл С
         if(p == n*2)
-        {   //Последующие логические выборы - средство для достижения поочередной записи(2 раза подряд в один и тот же файл) массивов в файлы С и Д
-            if(Np<2)
-            {
-                //записать массив c в файл С
-                writeToFileC(c, N, foutC);
-                p =0;
-                Np++;
-                //Заходим в функцию чтобы продолжить слияние при опустошенном массиве С
-                merge(a, i, b,j, c, p, N/2);
-            }
-            else if(Np>=2 && Np<4)
-            {
-                //записать массив d в файл D
-                writeToFileD(c, N, foutD);
-                p =0;
-                Np++;
-                if(Np>=4)
-                {
-                    Np=0;
-                    merge(a, i, b,j, c, p, N/2);
-                }
-                else
-                    merge(a, i, b,j, c, p, N/2);
-            }
+        {
+            //Последующие логические выборы - средство для достижения поочередной записи(2 раза подряд в один и тот же файл) массивов в файлы С и Д
+            //Функция записывания отрезка размером 5 в файлы С и Д
+            writeFromFrrayC(foutC, foutD, c, N, a, i, b, j,p );
         }
         //если указатель дошел до конца массива а(то есть i указывает на последний элемент)-значит массив полностью прочитан и его надо перезаписать
         else if(i == n)
         {
             //перезапись массива а
-            a = readFromFileA(finA, N/2);
+            readFromFileA(finA, N/2, a);
             i = 0;
-            merge(a, i, b,j, c, p, N/2);
+            merge(a, i, b,j, c, p, N/2, foutC, foutD, N);
         }
         //если указатель дошел до конца массива б-значит массив полностью прочитан и его надо перезаписать
         else if(j == n)
         {
             //перезапись массива b
-            b = readFromFileA(finA, N/2);
+            readFromFileB(finA, N/2, b);
             j = 0;
-            merge(a, i, b,j, c, p, N/2);
+            merge(a, i, b,j, c, p, N/2, foutC, foutD, N);
         }
-        //delete[] d;
 
-        finA.close();
-        finB.close();
-        foutC.close();
-        foutD.close();
 
-        delete[] a;
-        delete[] b;
-        delete[] c;
+        //delete[] a;
+        //delete[] b;
+        //delete[] c;
     }
+
+
+
+    finA.close();
+    finB.close();
+    foutC.close();
+    foutD.close();
 ////////////
     return 0;
 }
